c13b4b2813141c2c164326f3badc558e66b3bfd7
Kalen <kalen@krworks.com>
2021-12-11 00:35:06 -0700

    Add package.json & products

---
+++ products/dev/lib/mac/x86_64/_objs/minimal_logging/minimal_logging.d
@@ -0,0 +1,6 @@
+bazel-out/darwin-opt/bin/tensorflow/lite/_objs/minimal_logging/minimal_logging.o: \
+  tensorflow/lite/minimal_logging.cc tensorflow/lite/minimal_logging.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/c++/v1/cstdarg \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/c++/v1/__config \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/pthread.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/13.0.0/include/stdarg.h
---
+++ products/dev/lib/mac/x86_64/core/api/_objs/error_reporter/error_reporter.d
@@ -0,0 +1,7 @@
+bazel-out/darwin-opt/bin/tensorflow/lite/core/api/_objs/error_reporter/error_reporter.o: \
+  tensorflow/lite/core/api/error_reporter.cc \
+  tensorflow/lite/core/api/error_reporter.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/c++/v1/cstdarg \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/c++/v1/__config \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/pthread.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/13.0.0/include/stdarg.h
